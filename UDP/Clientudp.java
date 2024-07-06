c99f1f5a8bcf911ff4e8f48d6d6f55abf5c67511
akhilkajayaghosh <akhilkajayaghosh@gmail.com>
2024-07-06 18:41:39 +0530

    initial

--- UDP/Clientudp.java
+++ UDP/Clientudp.java
@@ -1,67 +1,67 @@
 import java.io.*;import java.net.*;
 public class Clientudp extends Thread
 {
-Thread t1,t2;
-DatagramSocket client;
-DatagramPacket dp;
-BufferedReader br;
-byte[] send=new byte[20];
-byte[] rec=new byte[20];
-InetAddress ip;
-String str,a;
-Clientudp()
-{
-try
-{
-client=new DatagramSocket();
-ip=InetAddress.getLocalHost();
-br=new BufferedReader(new InputStreamReader(System.in));
-}
-catch(Exception e)
-{
-System.out.println("error :"+e);
-}
-}
-public void run()
-{
-while(true)
-{
-if(Thread.currentThread()==t1)
-{
-try
-{
-dp=new DatagramPacket(rec,rec.length);
-client.receive(dp);
-str=new String(rec);
-System.out.println("from server:"+str);
-}
-catch(Exception e)
-{
-System.out.println("error :"+e);
-}
-}
-else
-{
-try
-{
-a=br.readLine();
-send=a.getBytes();
-dp=new DatagramPacket(send,send.length,ip,5555);
-client.send(dp);
-}
-catch(Exception e)
-{
-System.out.println("error :"+e);
-}
-}
-}
-}
-public static void main(String args[])
-{
-Clientudp c=new Clientudp();
-c.t1=new Thread(c);
-c.t2=new Thread(c);
-c.t1.start();
-c.t2.start();
-}
+	Thread t1,t2;
+	DatagramSocket client;
+	DatagramPacket dp;
+	BufferedReader br;
+	byte[] send=new byte[20];
+	byte[] rec=new byte[20];
+	InetAddress ip;
+	String str,a;
+	Clientudp()
+	{
+		try
+		{
+			client=new DatagramSocket();
+			ip=InetAddress.getLocalHost();
+			br=new BufferedReader(new InputStreamReader(System.in));
+		}
+		catch(Exception e)
+		{
+			System.out.println("error :"+e);
+		}
+	}
+	public void run()
+	{
+		while(true)
+		{
+			if(Thread.currentThread()==t1)
+			{
+				try
+				{
+					dp=new DatagramPacket(rec,rec.length);
+					client.receive(dp);
+					str=new String(rec);
+					System.out.println("from server:"+str);
+				}
+				catch(Exception e)
+				{
+					System.out.println("error :"+e);
+				}
+			}
+			else
+			{
+				try
+				{
+					a=br.readLine();
+					send=a.getBytes();
+					dp=new DatagramPacket(send,send.length,ip,5555);
+					client.send(dp);
+				}
+				catch(Exception e)
+				{
+					System.out.println("error :"+e);
+				}
+			}
+		}
+	}
+	public static void main(String args[])
+	{
+		Clientudp c=new Clientudp();
+		c.t1=new Thread(c);
+		c.t2=new Thread(c);
+		c.t1.start();
+		c.t2.start();
+	}
 }
--- UDP/Serverudp.java
+++ UDP/Serverudp.java
@@ -2,71 +2,71 @@ import java.io.*;
 import java.net.*;
 public class Serverudp extends Thread
 {
-Thread t1,t2;
-BufferedReader br;
-byte[] rec;
-byte[] send=new byte[20];
-DatagramSocket server;
-DatagramPacket dp=null;
-public static InetAddress ip;
-public static int port;
-String str,a;
-Serverudp()
-{
-try
-{
-server=new DatagramSocket(5555);
-br=new BufferedReader(new InputStreamReader(System.in));
-}
-catch(Exception e)
-{
-System.out.println("error :"+e);
-}
-}
-public void run()
-{
-while(true)
-{
-if(Thread.currentThread()==t1)
-{
-try
-{
-rec=new byte[20];
-dp=new DatagramPacket(rec,rec.length);
-server.receive(dp);
-ip=dp.getAddress();
-port=dp.getPort();
-str=new String(rec);
-System.out.println("from client:"+str);
-}
-catch(Exception e)
-{
-System.out.println("error :"+e);
-}
-}
-else
-{
-try
-{
-a=br.readLine();
-send=a.getBytes();
-dp=new
-DatagramPacket(send,send.length,ip,port);
-server.send(dp);
-}
-catch(Exception e)
-{
-System.out.println("error :"+e);
-}
-}
-}
-}
-public static void main(String args[])
-{
-Serverudp s=new Serverudp();
-s.t1=new Thread(s);
-s.t2=new Thread(s);
-s.t1.start();
-s.t2.start();
-}
+	Thread t1,t2;
+	BufferedReader br;
+	byte[] rec;
+	byte[] send=new byte[20];
+	DatagramSocket server;
+	DatagramPacket dp=null;
+	public static InetAddress ip;
+	public static int port;
+	String str,a;
+	Serverudp()
+	{
+		try
+		{
+			server=new DatagramSocket(5555);
+			br=new BufferedReader(new InputStreamReader(System.in));
+		}
+		catch(Exception e)
+		{
+			System.out.println("error :"+e);
+		}
+	}
+	public void run()
+	{
+		while(true)
+		{
+			if(Thread.currentThread()==t1)
+			{
+				try
+				{
+					rec=new byte[20];
+					dp=new DatagramPacket(rec,rec.length);
+					server.receive(dp);
+					ip=dp.getAddress();
+					port=dp.getPort();
+					str=new String(rec);
+					System.out.println("from client:"+str);
+				}
+				catch(Exception e)
+				{
+					System.out.println("error :"+e);
+				}
+			}
+			else
+			{
+				try
+				{
+					a=br.readLine();
+					send=a.getBytes();
+					dp=new
+					DatagramPacket(send,send.length,ip,port);
+					server.send(dp);
+				}
+				catch(Exception e)
+				{
+					System.out.println("error :"+e);
+				}
+			}
+		}
+	}
+	public static void main(String args[])
+	{
+		Serverudp s=new Serverudp();
+		s.t1=new Thread(s);
+		s.t2=new Thread(s);
+		s.t1.start();
+		s.t2.start();
+	}
 }
