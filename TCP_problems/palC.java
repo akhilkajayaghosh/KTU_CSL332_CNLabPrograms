e529967018b17589b0b124c18aea360c6f08ab7b
akhilkajayaghosh <akhilkajayaghosh@gmail.com>
2024-07-07 10:28:33 +0530

    initial

---
+++ TCP_problems/palC.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+public class palC extends Thread
+{
+	Thread t1,t2;
+	BufferedReader br,buf;
+	String str,a;
+	PrintWriter pout;
+	Socket client;
+	palC()
+	{
+		try
+		{
+			client=new Socket("localhost",5555);
+			br=new BufferedReader(new InputStreamReader(System.in));
+			buf=new BufferedReader(new InputStreamReader(client.getInputStream()));
+			pout=new PrintWriter(client.getOutputStream(),true);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public void run()
+	{
+		while(true)
+		{
+			if(Thread.currentThread()==t1)
+			{
+				try
+				{
+					str=br.readLine();
+					pout.println(str);
+				}
+				catch(Exception e)
+				{
+					System.out.println(e);
+				}
+			}
+			else
+			{
+				try
+				{
+					a=buf.readLine();
+					System.out.println("From Server "+a);
+				}
+				catch(Exception e)
+				{
+					System.out.println(e);
+				}
+			}
+		}
+	}
+	public static void main(String []args)
+	{
+		palC s=new palC();
+		s.t1=new Thread(s);
+		s.t2=new Thread(s);
+		s.t1.start();
+		s.t2.start();
+	}
+}
---
+++ TCP_problems/palS.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+public class palS extends Thread
+{
+	Thread t1,t2;
+	BufferedReader br,buf;
+	String str,a;
+	PrintWriter pout;
+	ServerSocket server;
+	Socket client;
+	palS()
+	{
+		try
+		{
+			server=new ServerSocket(5555);
+			client=server.accept();
+			br=new BufferedReader(new InputStreamReader(System.in));
+			buf=new BufferedReader(new InputStreamReader(client.getInputStream()));
+			pout=new PrintWriter(client.getOutputStream(),true);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public void run()
+	{
+			try
+			{
+				a=buf.readLine();
+				System.out.println("From CLient "+a);
+				StringBuffer bf=new StringBuffer(a);
+				String val=bf.reverse().toString();
+				if(val.equals(a))
+					str="The String is Palindrome";
+				else
+					str=val;
+				pout.println(str);
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+	}
+	public static void main(String []args)
+	{
+		palS s=new palS();
+		s.t1=new Thread(s);
+		s.t2=new Thread(s);
+		s.t1.start();
+		s.t2.start();
+	}
+}
